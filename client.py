8fc4429795750adfac3577aaa5450bad1f32ccc4
Matt <mrmace@umail.iu.edu>
2020-02-01 20:59:41 -0500

    Added Communication Aspects
    Added the communication from client to server and server back to client.

--- client.py
+++ client.py
@@ -1,11 +1,24 @@
 import socket
 
-host = socket.gethostname()  
+host = socket.gethostname()
 port = 9500
+cts = True
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host,port))
-    s.sendall(b"Hi")
-    data = s.recv(1024)
+        s.connect((host,port))
+        while cts == True:
+            msg = input("Type what you would like to send ")
 
-print('Received', repr(data))
+            s.sendall(msg.encode('ascii'))
+            print("You: {}".format(msg))
+            response = s.recv(1024)
+            responseBack = response.decode('ascii')
+
+            if responseBack == "Hi":
+                print("Server: {}".format(responseBack))
+            else:
+                print("Server: {}".format(responseBack))
+
+                cts = False
+                s.close()
+print("Connection Ended!")
--- server.py
+++ server.py
@@ -1,16 +1,31 @@
 import socket
 
-host = socket.gethostname()  
+host = socket.gethostname()
 port = 9500
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host,port))
-    s.listen()
+    s.bind((host,port))
+    s.listen(2)
     connection, addr = s.accept()
     with connection:
-        print('Connected by', addr)
+
         while True:
-            data = conn.recv(1024)
+            data = connection.recv(1024)
+            updatedData = data.decode('ascii')
+            if updatedData == "Hello":
+                respond = "Hi"
+                print("Client: {}".format(updatedData))
+                connection.send(respond.encode('ascii'))
+                print("You: {}".format(respond))
+            elif updatedData != "Hello" and updatedData != "":
+                respond = "Goodbye"
+                print("Client: {}".format(updatedData))
+                connection.send(respond.encode('ascii'))
+                print("You: {}".format(respond))
+
+            else:
+                print("Connection Ended!")
+
             if not data:
                 break
-            connection.sendall(data)
+            s.close()
